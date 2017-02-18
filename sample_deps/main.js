cccaa6d2daf1df90edc9e7f4d348f1878e1eb703
borsch <oleh.kurpiak@gmail.com>
2017-02-18 17:31:06 +0200

    add sample

---
+++ sample_deps/main.js
@@ -0,0 +1,12 @@
+CarouselInit('.carousel-main-container', {
+    directions: [
+        'left',
+        'down',
+        'right',
+        'down'
+    ],
+    easing: Math.sin,
+    time: 500
+});
+
+hljs.highlightBlock(document.getElementById('js_sample'));
